3ad4e47bba659c168ed8c743df03c93a2e295957
pompororo <praweena.sare@bumail.net>
2022-11-09 19:31:21 +0700

    Add Inventory Display

---
+++ Assets/Script/InventoryDisplay.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryDisplay : MonoBehaviour
+{
+    public static InventorySystem Instance;
+    public  GameObject InventoryBox;
+    public  GameObject DetailBox;
+
+    public void ShowInventory() 
+    {
+        if (InventoryBox != null)
+        {
+            bool isActive = InventoryBox.activeSelf;
+            InventoryBox.SetActive(!isActive);
+        }
+    }
+    
+    public void CloseInventory()
+    {
+        InventoryBox.SetActive(false);
+    }
+    
+    public void ShowDetailBox()
+    {
+        DetailBox.SetActive(true);
+    }
+    
+    public void CloseDetailBox()
+    {
+        DetailBox.SetActive(false);
+    }
+}
