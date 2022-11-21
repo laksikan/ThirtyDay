4104d6486052c23bf7ba3cf81b57979faf9c102c
pompororo <praweena.sare@bumail.net>
2022-11-22 03:44:04 +0700

    Add result of event to debug log

--- Assets/Event/EventDisplay.cs
+++ Assets/Event/EventDisplay.cs
@@ -5,20 +5,85 @@ using UnityEngine;
 public class EventDisplay : MonoBehaviour
 {
     public GameObject ExplosionEvent;
+    public GameObject ChoiceE1;
+    public GameObject ChoiceE2;
+    public GameObject ChoiceE3;
+    public GameObject RefrigeratorEvent;
+    public GameObject ChoiceR1;
+    public GameObject ChoiceR2;
+    public GameObject ChoiceR3;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int ChoiceExplosion;
+    public int ChoiceRefrigerator;
 
-    // Update is called once per frame
+    //-------------------Result of main event--------------------------//
+    public void ExplosionOption1()
+    {
+        Debug.Log("Battle with Bandit");
+        ChoiceExplosion = 1;
+    }
+    
+    public void ExplosionOption2()
+    {
+        Debug.Log("Lost 1 Turn");
+        ChoiceExplosion = 2;
+    }
+    
+    public void ExplosionOption3()
+    {
+        Debug.Log("Nothing Happened -> Continue Travelling");
+        ChoiceExplosion = 3;
+    }
+    
+    //---------------------Result of second event-----------------------//
+    public void RefrigeratorOption1()
+    {
+        Debug.Log("30% -> Nothing Happened\n" +
+                  "30% -> Battle with Giant Cockroach\n" +
+                  "30% -> Battle with Junkie\n" +
+                  "10% -> Found the village\n");
+        ChoiceRefrigerator = 1;
+    }
+    
+    public void RefrigeratorOption2()
+    {
+        Debug.Log("Energy Point -18 \n" +
+                  "Weapon Duration -3\n" +
+                  "Meat +2\n");
+        ChoiceRefrigerator = 2;
+    }
+    
+    public void RefrigeratorOption3()
+    {
+        Debug.Log("Nothing Happened -> Continue Travelling");
+        ChoiceRefrigerator = 3;
+    }
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Main Event Show up");
             ExplosionEvent.SetActive(true);
+        }
+
+        if ( ChoiceExplosion >= 1)
+        {
+            ExplosionEvent.SetActive(false);
+            RefrigeratorShowUp();
+        }
+
+        void RefrigeratorShowUp()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Refrigerator Event Show up");
+                RefrigeratorEvent.SetActive(true);
+            }
+            if ( ChoiceRefrigerator >= 1)
+            {
+                RefrigeratorEvent.SetActive(false);
+            }
         }
     }
 }
